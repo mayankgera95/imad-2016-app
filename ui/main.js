df4d1e3a9dcfb657a9d173b643c75898a197aeb1
mayankgera95 <mayank.gera95@gmail.com>
2016-09-23 15:54:28 +0530

    added main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+//alert
+alert("this is JS");
+//change text
+var element = document.getElementById('main-text');
+element.innerHTML='New Value';
+
+//move image
+var img = document.getElementById('madi');
+var marginLeft =0;
+function moveRight(){
+	marginLeft = marginLeft + 1;
+	img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function () {
+	var interval =setInterval(moveRight ,50);
+	
+};
