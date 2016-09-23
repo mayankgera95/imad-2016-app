d4dcd870d25b30a36996c6152862817d6349cb52
mayankgera95 <mayank.gera95@gmail.com>
2016-09-24 04:40:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,5 +42,21 @@ button.onclick = function(){
     request.open('GET',"http://mayankgera95.imad.hasura-app.io/counter",true);
     request.send(null);
     
+    //submit name
     
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function(){
+        //make req to server and send name
+        
+        //capture a list of names and render it as list
+        var names = ['name1','name2','name3','name4'];
+        var list = '';
+        for(i=0;i<names.length;i++){
+            list += '<li>' + names[i] + '</li>';
+        }
+        var ul = document.getElementById('namelist');
+        ul.innerHTML = list;
+    }
 };
