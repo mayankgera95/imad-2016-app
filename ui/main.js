7f4837c190a649a9b28c0748abf50d141e4f67cf
mayankgera95 <mayank.gera95@gmail.com>
2016-09-24 05:43:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -75,7 +75,7 @@ button.onclick = function(){
     //make the req
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET','http://mayankgera95.imad.hasura-app.io/submit-name?name' + name,true);
+    request.open('GET','http://mayankgera95.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };
         
