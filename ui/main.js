22f93edd74a3d7f1d479e2e7ef49f9f0a49067a3
mayankgera95 <mayank.gera95@gmail.com>
2016-09-24 05:32:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,15 +49,34 @@ button.onclick = function(){
     var name = nameInput.value;
     var submit = document.getElementById('submit_btn');
     submit.onclick = function(){
-        //make req to server and send name
         
-        //capture a list of names and render it as list
-        var names = ['name1','name2','name3','name4'];
-        var list = '';
-        for(var i=0 ; i< names.length;i++){
-            list += '<li>' + names[i] + '</li>';
-        }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
+        //create req object
+    var request= new XMLHttpRequest();
+    //capture response and store it in variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                //capture a list of names and render it as list
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list = '';
+                    for(var i=0 ; i< names.length;i++){
+                    list += '<li>' + names[i] + '</li>';
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+                }
+                
+            }
+        
+        //not done yet
+        
     };
-
+    
+    //make the req
+    request.open('GET','http://mayankgera95.imad.hasura-app.io/submit-name?name' + name,true);
+    request.send(null);
+};
+        
+        
